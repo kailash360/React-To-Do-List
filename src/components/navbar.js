ae20841d8ac6eff7e4c978262ae6dc59855d8575
kailash360 <kailashkejriwal21@gmail.com>
2021-07-11 05:06:47 +0530

    Added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../static/navbar.css'
+
+function Navbar(){
+    return(
+        <>
+        <div class="navbar">
+            <p>To-Do List</p>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
