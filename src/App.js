7b712204152fb4fa76e24aafaa2fcc97b487954d
kailash360 <kailashkejriwal21@gmail.com>
2021-07-11 05:24:00 +0530

    Added input section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './static/App.css';
-import Navbar from './components/navbar.js'
+import Navbar from './components/navbar.js';
+import Input from './components/input-box.js';
 
 function App() {
   return (
+  <>
     <Navbar></Navbar>
+    <Input></Input>
+  </>
   );
 }
 
---
+++ src/components/input-box.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../static/input.css'
+
+function Input(){
+
+
+    return (
+        <>
+        <div className="input-container">
+            <input type="text" />
+            <button type="button">Submit</button>
+        </div>
+        
+        </>
+    )
+}
+
+export default Input;
