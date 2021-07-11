8c02c0501365b6f8b1081168493a6da8ea441b24
kailash360 <kailashkejriwal21@gmail.com>
2021-07-11 23:56:56 +0530

    Integrated with mainpage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import React ,{ useState } from 'react'
 import logo from './logo.svg';
 import './static/App.css';
+import './static/list.css';
 import Navbar from './components/navbar.js';
 import Input from './components/input-box.js';
+import List from './components/list.js'
+import Row from './components/row.js'
+
 
 function App() {
-  return (
+
+  const [todo_list, update] = useState([])
+
+  const add_to_list=(new_task)=>{
+    update([...todo_list,new_task])
+  }
+
+  const update_list=(task)=>{
+    update(todo_list.filter(item=>{
+      return item.desc!==task
+    }))
+  }
+
+  console.log(todo_list)
+
+  return( 
   <>
     <Navbar></Navbar>
-    <Input></Input>
+    <Input list={todo_list} add_to_list={add_to_list}></Input>
+    <List  update_list={update_list} todo_list={todo_list} className="list-container"></List>
   </>
-  );
+  )
 }
 
 export default App;
