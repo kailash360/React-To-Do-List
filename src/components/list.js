1d0ccc4fe4afd0f5406df531511d039fd1e8c30a
kailash360 <kailashkejriwal21@gmail.com>
2021-07-12 01:22:25 +0530

    Minor changes

--- src/components/list.js
+++ src/components/list.js
@@ -7,11 +7,13 @@ function List({update_list,todo_list}){
 
     return(
         <div className="list-container">
-        {todo_list.map(item=>{
-            return(
-                <><Row task={item.desc} update_list={update_list} key={item.key}></Row></>
-            )
-        })}
+            <p className="list-heading">My To-Dos</p>
+        {  todo_list.length>0
+            ?todo_list.map(item=>{
+                return(<><Row task={item.desc} update_list={update_list} key={item.key}></Row></>)
+            })
+            : <div className="empty-list">Nothing to show here..</div>
+        }
         </div>
     )
 }
