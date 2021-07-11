f53d410d87b259c70169c572bda4e6ed0482703f
kailash360 <kailashkejriwal21@gmail.com>
2021-07-11 23:56:41 +0530

    Added input box for todos

--- src/components/input-box.js
+++ src/components/input-box.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import '../static/input.css'
+import List from '../components/list.js'
 
-function Input(){
-
-
+function Input({list,add_to_list}){
+     
+    let new_task;
     return (
         <>
         <div className="input-container">
-            <input type="text" />
-            <button type="button">Submit</button>
+            <input type="text" onChange={(e)=>{
+                new_task={
+                    key:list.length+1,
+                    desc:e.target.value
+                }
+            }} />
+            <button type="button" onClick={()=>{
+                add_to_list(new_task)
+            }} >Submit</button>
         </div>
         
         </>
